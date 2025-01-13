baed5807eec38e7f979e1bfd28bccfa3ac8e4bca
Adil Hussain <silver@Adils-MacBook-Air.local>
2025-01-13 19:17:25 +0530

    input logic bug fix

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ function findLeastChocolate() {
     alert("Please Enter Chocolate Sizes.");
     return;
   }
-  chocolateSizes.sort((a, b) => a - b);
-  if (chocolateSizes[0] < chocolateSizes[1]) {
-    chocolateSizes.reverse();
-  }
+  chocolateSizes.sort(function (a, b) {
+    return a - b;
+  });
+  chocolateSizes.reverse();
 
   let maxEachChocolate = [];
 
